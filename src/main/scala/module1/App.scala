77bb9ebb59cb5e882624dbe74b66552ad0d9601d
limar63 <lehalim2008-limar@yandex.ru>
2021-08-11 03:11:38 +0400

    Updating to proper file structure

--- src/main/scala/module1/App.scala
+++ src/main/scala/module1/App.scala
@@ -1,10 +1,10 @@
 package module1
-import type_system.Rectangle
+
 
 object App {
 
   def main(args: Array[String]): Unit = {
-      println(type_system.v.foo())
-      println(type_system.v1.foo())
+    OptionTests.startTesting
+    ListTesting.startTesting
   }
 }
